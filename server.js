c3b307cf6ec276e2ecbdc4a7db63615ab694840c
hsnertas <hsnertas@gmail.com>
2020-10-13 22:35:25 -0400

    mondoDB connected

--- server.js
+++ server.js
@@ -1,12 +1,14 @@
 // importing
 import express from 'express';
 import mongoose from 'mongoose';
+import connectDB from './config/db.js';
 // app config
 const app = express();
 const port = process.env.PORT || 5000;
 // middleware
 
 // db config
+connectDB();
 // api routes
 app.get('/', (req, res) => res.send('Hello World'));
 // listen
