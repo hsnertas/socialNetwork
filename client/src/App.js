73b90dbdb3d95b55b5274b0e11d8f4fd0da44523
hsnertas <hsnertas@gmail.com>
2020-10-21 21:17:00 -0400

    react router set up

--- client/src/App.js
+++ client/src/App.js
@@ -1,14 +1,23 @@
 import React, { Fragment } from 'react';
+import { BrowserRouter as Router, Route, Switch } from 'react-router-dom';
 import Navbar from './components/layout/Navbar';
 import Landing from './components/layout/Landing';
+import Login from './components/auth/Login';
+import Register from './components/auth/Reqister';
 import './App.css';
 
 function App() {
   return (
-    <Fragment>
-      <Navbar />
-      <Landing />
-    </Fragment>
+    <Router>
+      <Fragment>
+        <Navbar />
+        <Route exact path='/' component={Landing} />
+        <section className='container'>
+          <Route exact path='/register' component={Register} />
+          <Route exact path='/login' component={Login} />
+        </section>
+      </Fragment>
+    </Router>
   );
 }
 
---
+++ client/src/components/auth/Reqister.js
@@ -0,0 +1,11 @@
+import React from 'react';
+
+const Reqister = () => {
+  return (
+    <div>
+      <h1>Register Page</h1>
+    </div>
+  );
+};
+
+export default Reqister;
