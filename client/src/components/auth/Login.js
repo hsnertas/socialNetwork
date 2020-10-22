90bcdd3b924448889685fc9b2b81ab513ca89b82
hsnertas <hsnertas@gmail.com>
2020-10-22 02:43:49 -0400

    login and register components working on client side

--- client/src/components/auth/Login.js
+++ client/src/components/auth/Login.js
@@ -1,6 +1,38 @@
 import React, { Fragment, useState } from 'react';
-
+import { Link } from 'react-router-dom';
 const Login = () => {
+  const [formData, setFormData] = useState({
+    email: '',
+    password: '',
+  });
+  const { email, password } = formData;
+  const onChange = e =>
+    setFormData({ ...formData, [e.target.name]: e.target.value });
+
+  const onSubmit = async e => {
+    e.preventDefault();
+    console.log(formData);
+    // Without Redux
+    //   const newUser = {
+    //     name,
+    //     email,
+    //     password,
+    //   };
+
+    //   try {
+    //     const config = {
+    //       headers: {
+    //         'Content-Type': 'application/json',
+    //       },
+    //     };
+
+    //     const body = JSON.stringify(newUser);
+    //     const res = await axios.post('/api/users', body, config);
+    //     console.log(res.data);
+    //   } catch (error) {
+    //     console.error(error.message);
+    //   }
+  };
   return (
     <Fragment>
       <div className='alert alert-danger'>Invalid credentials</div>
@@ -8,22 +40,31 @@ const Login = () => {
       <p className='lead'>
         <i className='fas fa-user'></i> Sign into Your Account
       </p>
-      <form className='form' action='dashboard.html'>
+      <form className='form' onSubmit={e => onSubmit(e)}>
         <div className='form-group'>
           <input
             type='email'
             placeholder='Email Address'
             name='email'
+            value={email}
+            onChange={e => onChange(e)}
             required
           />
         </div>
         <div className='form-group'>
-          <input type='password' placeholder='Password' name='password' />
+          <input
+            type='password'
+            placeholder='Password'
+            name='password'
+            value={password}
+            onChange={e => onChange(e)}
+            pattern='(?=.*\d)(?=.*[a-z])(?=.*[A-Z]).{6,16}'
+          />
         </div>
         <input type='submit' className='btn btn-primary' value='Login' />
       </form>
       <p className='my-1'>
-        Don't have an account? <a href='register.html'>Sign Up</a>
+        Don't have an account? <Link to='/register'>Sign Up</Link>
       </p>
     </Fragment>
   );
--- client/src/components/auth/Reqister.js
+++ client/src/components/auth/Reqister.js
@@ -1,5 +1,5 @@
 import React, { Fragment, useState } from 'react';
-
+import { Link } from 'react-router-dom';
 const Reqister = () => {
   const [formData, setFormData] = useState({
     name: '',
@@ -11,12 +11,32 @@ const Reqister = () => {
   const onChange = e =>
     setFormData({ ...formData, [e.target.name]: e.target.value });
 
-  const onSubmit = e => {
+  const onSubmit = async e => {
     e.preventDefault();
     if (password !== password2) {
       console.log('Password do not match');
     } else {
-      console.log(formData);
+      console.log('Success');
+      // Without Redux
+      //   const newUser = {
+      //     name,
+      //     email,
+      //     password,
+      //   };
+
+      //   try {
+      //     const config = {
+      //       headers: {
+      //         'Content-Type': 'application/json',
+      //       },
+      //     };
+
+      //     const body = JSON.stringify(newUser);
+      //     const res = await axios.post('/api/users', body, config);
+      //     console.log(res.data);
+      //   } catch (error) {
+      //     console.error(error.message);
+      //   }
     }
   };
   return (
@@ -72,7 +92,7 @@ const Reqister = () => {
         <input type='submit' className='btn btn-primary' value='Register' />
       </form>
       <p className='my-1'>
-        Already have an account? <a href='login.html'>Sign In</a>
+        Already have an account? <Link to='/login'>Sign In</Link>
       </p>
     </Fragment>
   );
